b46568f89f0e949c941b7d2e70e2a5573d2d7bbd
marcin-pwr <200638@student.pwr.edu.pl>
2017-04-01 12:50:08 +0200

    Poprawki, wystarczy nazwa funkcji do badań. Ilość wymiarów jest uzależniona od funkcji, wydruk zawsze odbywa się dla 2 wymiarów. Optymalizacja zawsze dla tylu ile jest dostępnych i w domyślnych boundsach. Chodzi o to by mieć optimum z benchmarka a ono jest dla wszystkich wymiarów w domyślnych boundsach.

--- unold_lab12/assets/skrypt_lab12.R
+++ unold_lab12/assets/skrypt_lab12.R
@@ -5,100 +5,68 @@ require("GA")
 require("globalOptTests")
 require("rgl")
 
-graphs <- TRUE
-
-# Function proxies ----
-ackley <- function(xx)
-{
-  return(goTest(par=c(xx, rep(0, 10-length(xx))), fnName="Ackleys", checkDim = TRUE))
-}
-
-branin <- function(xx)
-{
-  return(goTest(par=c(xx), fnName="Branin", checkDim = TRUE))
-}
-
-schwef <- function(xx)
-{
-  return(goTest(par=c(xx,0,0,0,0,0,0,0,0), fnName="Schwefel", checkDim = TRUE))
-}
-
 # Params ----
 
-isSingleTest = FALSE
-n <- 7               # default 7
-GAPopulation <- 50  # default 500
-GAIterations <- 5   # default 50
+n <- 2               # default 7
+GAPopulation <- 10  # default 500
+GAIterations <- 2   # default 50
 GAMutations <- 0.1   # % (def 0.1)
 GACrossovers <- 0.8  # % (def 0.8)
 
-# Ackleys 2D function ----
+isSingleTest <- FALSE
+graphs <- TRUE
+quality <- 100 #graph probes
 
-# xFrom <- -32.768
-# xTo <- 32.768
-# f <- Vectorize(ackley)
-# is3D = FALSE
+# Functions ----
 
-# Ackleys 3D function ----
-
-d <- 0.8
-B <- matrix(c(-32.768, -32.768, 32.768, 32.768), nrow=2, ncol=2, byrow = TRUE)
-f <- function(x, y) ackley(c(x,y))
-is3D = TRUE
-
-# Branin function ----
-
-# d <- 0.5
-# B <- matrix(c(-5, 0, 10, 15), nrow=2, ncol=2, byrow = TRUE)
-# f <- function(x, y) branin(c(x,y))
-# is3D = TRUE
-
-# Schwefel function ----
-
-# d <- 5
-# B <- matrix(c(-500, -500, 500, 500), nrow=2, ncol=2, byrow = TRUE)
-# f <- function(x, y) schwef(c(x,y))
-# is3D = TRUE
+#funcName <- "Branin" #2d
+#funcName <- "Gulf" #3d
+#funcName <- "CosMix4" #4d
+funcName <- "EMichalewicz" #5d
+#funcName <- "Hartman6" #6d
+#funcName <- "PriceTransistor" #9d
+#funcName <- "Schwefel" #10d
+#funcName <- "Zeldasine20" #20d
 
 # Processing ----
 
-if (is3D) {
-  x <- seq(B[1,1], B[2,1], by = d)
-  y <- seq(B[1,2], B[2,2], by = d)
-}
+dim <- getProblemDimen(funcName)
+B <- matrix(unlist(getDefaultBounds(funcName)),ncol=dim,byrow=TRUE)
+f <- function(xx) goTest(par=c(xx, rep(0, dim-length(xx))), fnName=funcName, checkDim = TRUE)
+globalOpt <- getGlobalOpt(funcName)
 
 if (graphs) {
-  if (is3D) {
-    z <- outer(x, y, Vectorize(f))
-    nbcol = 100
-    color = rev(rainbow(nbcol, start = 0/6, end = 4/6))
-    zcol  = cut(z, nbcol)
-    persp3d(x, y, z, theta=50, phi=25, expand=0.75, col=color[zcol],
-            ticktype="detailed",axes=TRUE)
-    persp3D(x, y, z, theta = -45, phi = 20, color.palette = jet.colors)
-  } else {
-    curve (f, xFrom, xTo)
-  }
+  xprobes <- abs(B[2,1] - B[1,1]) / quality
+  yprobes <- abs(B[2,2] - B[1,2]) / quality
+  x <- seq(B[1,1], B[2,1], by = xprobes)
+  y <- seq(B[1,2], B[2,2], by = yprobes)
+  z <- outer(x, y, Vectorize(function(x,y) f(c(x,y))))
+  nbcol = 100
+  color = rev(rainbow(nbcol, start = 0/6, end = 4/6))
+  zcol  = cut(z, nbcol)
+  persp3d(x, y, z, theta=50, phi=25, expand=0.75, col=color[zcol],
+          ticktype="detailed",axes=TRUE)
+  persp3D(x, y, z, theta = -45, phi = 20, color.palette = jet.colors)
 }
 
 if (isSingleTest) {
 
-  sum <- 0
   vector <- rep(NA,n)
   for (i in 1:n) {
-    if (is3D) {
-      GAmin <- ga(type = "real-valued", fitness =  function(x) -f(x[1],x[2]), min = c(B[1,1],B[1,2]), max = c(B[2,1],B[2,2]), popSize = GAPopulation, maxiter = GAIterations, pmutation=GAMutations, pcrossover = GACrossovers)
-      eval <- f(GAmin@solution[,1],GAmin@solution[,2])
-    } else {
-      GAmin <- ga(type = "real-valued", fitness =  function(x) -f(x), min = xFrom, max = xTo, popSize = GAPopulation, maxiter = GAIterations, pmutation=GAMutations, pcrossover = GACrossovers)
-      eval <- f(GAmin@solution[,1])
-    }
-    vector[i] <- eval
+    GAmin <- ga(type = "real-valued", fitness =  function(xx) -f(xx), 
+                min = c(B[1,]), max = c(B[2,]), 
+                popSize = GAPopulation, maxiter = GAIterations, 
+                pmutation = GAMutations, pcrossover = GACrossovers)
+    solution <- matrix(unlist(GAmin@solution),ncol=dim,byrow=TRUE)
+    vector[i] <- f(solution[1,])
   }
   result <- matrix(c(vector),nrow = n,ncol = 1)
   write.table(result, file = "resultsSingle.csv", row.names=FALSE, na="", col.names=FALSE, sep=";")
   
 } else {
+  
+  gMin <- .Machine$integer.max
+  gBest <- NA
   
   temp <- c()
   values <- c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0)
@@ -107,29 +75,32 @@ if (isSingleTest) {
     sum <- 0
     vector <- rep(NA,n)
     for (i in 1:n) {
-      if (is3D) {
-        GAmin <- ga(type = "real-valued", fitness =  function(x) -f(x[1],x[2]), min = c(B[1,1],B[1,2]), max = c(B[2,1],B[2,2]), popSize = GAPopulation, maxiter = GAIterations, pmutation=mutation, pcrossover = GACrossovers)
-        eval <- f(GAmin@solution[,1],GAmin@solution[,2])
-      } else {
-        GAmin <- ga(type = "real-valued", fitness =  function(x) -f(x), min = xFrom, max = xTo, popSize = GAPopulation, maxiter = GAIterations, pmutation=mutation, pcrossover = GACrossovers)
-        eval <- f(GAmin@solution[,1])
+      GAmin <- ga(type = "real-valued", 
+                  fitness =  function(xx) -f(xx), 
+                  min = c(B[1,]), max = c(B[2,]), 
+                  popSize = GAPopulation, maxiter = GAIterations, 
+                  pmutation = mutation, pcrossover = GACrossovers)
+      solution <- matrix(unlist(GAmin@solution),ncol=dim,byrow=TRUE)
+      eval <- f(solution[1,])
+      if (eval < gMin) {
+        gMin <- eval
+        gBest <- GAmin
       }
       sum <- sum + eval
       vector[i] <- eval
     }
     temp <- c(temp, vector)
-    avg <- sum / n
-    averages <- c(averages, avg)
+    averages <- c(averages, (sum / n))
   }
   result <- matrix(c(temp),nrow = n,ncol = length(values))
   write.table(result, file = "resultsMutations.csv", row.names=FALSE, na="", col.names=FALSE, sep=";")
   
   if (graphs) {
     plot(values, averages, 
-         main="Wartoœæ funkcji celu dla ró¿nych wartoœci P. mutacji", 
-         ylim=c(-0.05,max(averages)), 
-         type="l", col="red", xlab="parametry", ylab="wartoœæ funkcji")
-    abline(0,0, col="green")
+         main="Goal function value for different mutation probabilities", 
+         ylim=c(min(c(averages,globalOpt)),max(c(averages,globalOpt))), 
+         type="l", col="red", xlab="params", ylab="value")
+    abline(globalOpt,0, col="green")
   }
   
   temp <- c()
@@ -139,31 +110,34 @@ if (isSingleTest) {
     sum <- 0
     vector <- rep(NA,n)
     for (i in 1:n) {
-      if (is3D) {
-        GAmin <- ga(type = "real-valued", fitness =  function(x) -f(x[1],x[2]), min = c(B[1,1],B[1,2]), max = c(B[2,1],B[2,2]), popSize = GAPopulation, maxiter = GAIterations, pmutation=GAMutations, pcrossover = crossover)
-        eval <- f(GAmin@solution[,1],GAmin@solution[,2])
-      } else {
-        GAmin <- ga(type = "real-valued", fitness =  function(x) -f(x), min = xFrom, max = xTo, popSize = GAPopulation, maxiter = GAIterations, pmutation=GAMutations, pcrossover = crossover)
-        eval <- f(GAmin@solution[,1])
+      GAmin <- ga(type = "real-valued", 
+                  fitness =  function(xx) -f(xx), 
+                  min = c(B[1,]), max = c(B[2,]), 
+                  popSize = GAPopulation, maxiter = GAIterations, 
+                  pmutation = GAMutations, pcrossover = crossover)
+      solution <- matrix(unlist(GAmin@solution),ncol=dim,byrow=TRUE)
+      eval <- f(solution[1,])
+      if (eval < gMin) {
+        gMin <- eval
+        gBest <- GAmin
       }
       sum <- sum + eval
       vector[i] <- eval
     }
     temp <- c(temp, vector)
-    avg <- sum / n
-    averages <- c(averages, avg)
+    averages <- c(averages, (sum / n))
   }
   result <- matrix(c(temp),nrow = n,ncol = length(values))
   write.table(result, file = "resultsCrossover.csv", row.names=FALSE, na="", col.names=FALSE, sep=";")
-  
+
   if (graphs) {
-    plot(values, averages, 
-         main="Wartoœæ funkcji celu dla ró¿nych wartoœci P. krzy¿owania", 
-         ylim=c(-0.05,max(averages)), 
-         type="l", col="red", xlab="parametry", ylab="wartoœæ funkcji")
-    abline(0,0, col="green")
+    plot(values, averages,
+         main="Goal function value for different crossover probabilities", 
+         ylim=c(min(c(averages,globalOpt)),max(c(averages,globalOpt))), 
+         type="l", col="red", xlab="params", ylab="value")
+    abline(globalOpt,0, col="green")
   }
-  
+
   temp <- c()
   values <- c(0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5)
   averages <- c()
@@ -171,31 +145,34 @@ if (isSingleTest) {
     sum <- 0
     vector <- rep(NA,n)
     for (i in 1:n) {
-      if (is3D) {
-        GAmin <- ga(type = "real-valued", fitness =  function(x) -f(x[1],x[2]), min = c(B[1,1],B[1,2]), max = c(B[2,1],B[2,2]), popSize = GAPopulation, maxiter = GAIterations, pmutation=GAMutations, pcrossover = GACrossovers, elitism = elitism)
-        eval <- f(GAmin@solution[,1],GAmin@solution[,2])
-      } else {
-        GAmin <- ga(type = "real-valued", fitness =  function(x) -f(x), min = xFrom, max = xTo, popSize = GAPopulation, maxiter = GAIterations, pmutation=GAMutations, pcrossover = GACrossovers, elitism = elitism)
-        eval <- f(GAmin@solution[,1])
+      GAmin <- ga(type = "real-valued", 
+                  fitness =  function(xx) -f(xx), 
+                  min = c(B[1,]), max = c(B[2,]), 
+                  popSize = GAPopulation, maxiter = GAIterations, 
+                  pmutation = GAMutations, pcrossover = GACrossovers, elitism = elitism)
+      solution <- matrix(unlist(GAmin@solution),ncol=dim,byrow=TRUE)
+      eval <- f(solution[1,])
+      if (eval < gMin) {
+        gMin <- eval
+        gBest <- GAmin
       }
       sum <- sum + eval
       vector[i] <- eval
     }
     temp <- c(temp, vector)
-    avg <- sum / n
-    averages <- c(averages, avg)
+    averages <- c(averages, (sum / n))
   }
   result <- matrix(c(temp),nrow = n,ncol = length(values))
   write.table(result, file = "resultsElitism.csv", row.names=FALSE, na="", col.names=FALSE, sep=";")
-  
+
   if (graphs) {
-    plot(values, averages, 
-         main="Wartoœæ funkcji celu dla ró¿nych wartoœci elityzmu", 
-         ylim=c(-0.05,max(averages)), 
-         type="l", col="red", xlab="parametry", ylab="wartoœæ funkcji")
-    abline(0,0, col="green")
+    plot(values, averages,
+         main="Goal function value for different elitism", 
+         ylim=c(min(c(averages,globalOpt)),max(c(averages,globalOpt))), 
+         type="l", col="red", xlab="params", ylab="value")
+    abline(globalOpt,0, col="green")
   }
-  
+
   temp <- c()
   values <- c(100, 200, 300, 400, 500, 600, 700, 800, 900, 1000)
   averages <- c()
@@ -203,31 +180,34 @@ if (isSingleTest) {
     sum <- 0
     vector <- rep(NA,n)
     for (i in 1:n) {
-      if (is3D) {
-        GAmin <- ga(type = "real-valued", fitness =  function(x) -f(x[1],x[2]), min = c(B[1,1],B[1,2]), max = c(B[2,1],B[2,2]), popSize = population, maxiter = GAIterations, pmutation=GAMutations, pcrossover = GACrossovers)
-        eval <- f(GAmin@solution[,1],GAmin@solution[,2])
-      } else {
-        GAmin <- ga(type = "real-valued", fitness =  function(x) -f(x), min = xFrom, max = xTo, popSize = population, maxiter = GAIterations, pmutation=GAMutations, pcrossover = GACrossovers)
-        eval <- f(GAmin@solution[,1])
+      GAmin <- ga(type = "real-valued", 
+                  fitness =  function(xx) -f(xx), 
+                  min = c(B[1,]), max = c(B[2,]), 
+                  popSize = population, maxiter = GAIterations, 
+                  pmutation = GAMutations, pcrossover = GACrossovers)
+      solution <- matrix(unlist(GAmin@solution),ncol=dim,byrow=TRUE)
+      eval <- f(solution[1,])
+      if (eval < gMin) {
+        gMin <- eval
+        gBest <- GAmin
       }
       sum <- sum + eval
       vector[i] <- eval
     }
     temp <- c(temp, vector)
-    avg <- sum / n
-    averages <- c(averages, avg)
+    averages <- c(averages, (sum / n))
   }
   result <- matrix(c(temp),nrow = n,ncol = length(values))
   write.table(result, file = "resultsPopulation.csv", row.names=FALSE, na="", col.names=FALSE, sep=";")
-  
+
   if (graphs) {
-    plot(values, averages, 
-         main="Wartoœæ funkcji celu dla ró¿nych rozmiarów populacji", 
-         ylim=c(-0.05,max(averages)), 
-         type="l", col="red", xlab="parametry", ylab="wartoœæ funkcji")
-    abline(0,0, col="green")
+    plot(values, averages,
+         main="Goal function value for different population sizes", 
+         ylim=c(min(c(averages,globalOpt)),max(c(averages,globalOpt))), 
+         type="l", col="red", xlab="params", ylab="value")
+    abline(globalOpt,0, col="green")
   }
-  
+
   temp <- c()
   values <- c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
   averages <- c()
@@ -235,46 +215,44 @@ if (isSingleTest) {
     sum <- 0
     vector <- rep(NA,n)
     for (i in 1:n) {
-      if (is3D) {
-        GAmin <- ga(type = "real-valued", fitness =  function(x) -f(x[1],x[2]), min = c(B[1,1],B[1,2]), max = c(B[2,1],B[2,2]), popSize = GAPopulation, maxiter = iterations, pmutation=GAMutations, pcrossover = GACrossovers)
-        eval <- f(GAmin@solution[,1],GAmin@solution[,2])
-      } else {
-        GAmin <- ga(type = "real-valued", fitness =  function(x) -f(x), min = xFrom, max = xTo, popSize = GAPopulation, maxiter = iterations, pmutation=GAMutations, pcrossover = GACrossovers)
-        eval <- f(GAmin@solution[,1])
+      GAmin <- ga(type = "real-valued", 
+                  fitness =  function(xx) -f(xx), 
+                  min = c(B[1,]), max = c(B[2,]), 
+                  popSize = GAPopulation, maxiter = iterations, 
+                  pmutation = GAMutations, pcrossover = GACrossovers)
+      solution <- matrix(unlist(GAmin@solution),ncol=dim,byrow=TRUE)
+      eval <- f(solution[1,])
+      if (eval < gMin) {
+        gMin <- eval
+        gBest <- GAmin
       }
       sum <- sum + eval
       vector[i] <- eval
     }
     temp <- c(temp, vector)
-    avg <- sum / n
-    averages <- c(averages, avg)
+    averages <- c(averages, (sum / n))
   }
   result <- matrix(c(temp),nrow = n,ncol = 10)
   write.table(result, file = "resultsIterations.csv", row.names=FALSE, na="", col.names=FALSE, sep=";")
-  
+
   if (graphs) {
-    plot(values, averages, 
-         main="Wartoœæ funkcji celu dla ró¿nych iloœci iteracji", 
-         ylim=c(-0.05,max(averages)), 
-         type="l", col="red", xlab="parametry", ylab="wartoœæ funkcji")
-    abline(0,0, col="green")
+    plot(values, averages,
+         main="Goal function value for different iteration quantities", 
+         ylim=c(min(c(averages,globalOpt)),max(c(averages,globalOpt))), 
+         type="l", col="red", xlab="params", ylab="value")
+    abline(globalOpt,0, col="green")
   }
   
 }
 
 if (graphs) {
   summary(GAmin)
-  if (is3D) {
-    filled.contour(x, y, z, color.palette = jet.colors, nlevels = 24, 
-       plot.axes = { 
-         axis(1); 
-         axis(2);
-         points(GAmin@solution[,1], GAmin@solution[,2], pch = 3, cex = 5, col = "black", lwd = 2) 
-       }
-    )
-  } else {
-    points(GAmin@solution, f(GAmin@solution), col="red", pch=19)
-    abline(v=GAmin@solution, h=f(GAmin@solution), col="red", lty=2)
-  }
+  filled.contour(x, y, z, color.palette = jet.colors, nlevels = 24, 
+     plot.axes = { 
+       axis(1); 
+       axis(2);
+       points(solution[1,1], solution[1,2], pch = 3, cex = 5, col = "black", lwd = 2) 
+     }
+  )
   plot(GAmin)
 }
