e2718281fccee8224c1f693a531893ae01c3bd28
marcin-pwr <200638@student.pwr.edu.pl>
2017-04-02 23:56:36 +0200

    Wszystkie pomiary dla 3 rodzajów ustawień domyślnych. Wszystkie wykresy eksportowane bezpośrednio do plików.

--- unold_lab12/assets/skrypt_lab12.R
+++ unold_lab12/assets/skrypt_lab12.R
@@ -8,27 +8,32 @@ require("rgl")
 
 # Params ----
 
-n <- 10              # minimum 5
-GAPopulation <- 50   # default 50
-GAIterations <- 100  # default 100
-GAMutations <- 0.1   # default 0.1
-GACrossovers <- 0.8  # default 0.8
+nOfRuns <- 10 # minimum 5
 
-isSingleTest <- FALSE
+colors <- c("red", "blue", "orange")
+series <- c("Seria 1", "Seria 2", "Seria 3")
+
+baseParams = matrix(
+  c(0, 0, 50, 100, 1,
+    0.1, 0.8, 50, 100, 2,
+    0.1, 0.8, 25, 50, 3), 
+  # [mutations,crossovers,populations,iterations,color]*
+  nrow=3, ncol=5, byrow = TRUE)
+
 graphs <- TRUE
 quality <- 100 #graph resolutions
 
 mutationTests <- seq(0, 1, 0.1)
 crossoverTests <- seq(0, 1, 0.1)
-elitismTests <- seq(0, 1, 0.1)
 populationTests <- seq(10, 100, 5)
 iterationTests <- seq(10, 200, 10)
+elitismTests <- seq(0, 1, 0.1)
 
 # Functions ----
 
-#funcName <- "Branin" #2d
+funcName <- "Branin" #2d
 #funcName <- "Gulf" #3d
-funcName <- "CosMix4" #4d
+#funcName <- "CosMix4" #4d
 #funcName <- "EMichalewicz" #5d
 #funcName <- "Hartman6" #6d
 #funcName <- "PriceTransistor" #9d
@@ -37,12 +42,88 @@ funcName <- "CosMix4" #4d
 
 # Processing ----
 
+customMeasure <- function(fileName, graphName, values, mType, xlab, main) {
+  
+  gMin <- .Machine$integer.max
+  gBest <- NA
+  
+  temp <- c()
+  for (defRow in 1:nrow(baseParams)) {
+    averages <- c()
+    for (value in values) {
+      sum <- 0
+      for (i in 1:nOfRuns) {
+        GAmin <- ga(type = "real-valued",
+            fitness =  function(xx) -f(xx),
+            min = c(B[1,]), max = c(B[2,]),
+            popSize = if (mType == "pop") value else baseParams[defRow,3],
+            maxiter = if (mType == "itr") value else baseParams[defRow,4],
+            pmutation = if (mType == "mut") value else baseParams[defRow,1], 
+            pcrossover = if (mType == "crs") value else baseParams[defRow,2],
+            elitism = if (mType == "elt") value else max(1, round(baseParams[defRow,3] * 0.05)))
+        solution <- matrix(unlist(GAmin@solution),ncol=dim,byrow=TRUE)
+        eval <- f(solution[1,])
+        if (eval < gMin) {
+          gMin <- eval
+          gBest <- GAmin
+        }
+        sum <- sum + eval
+      }
+      averages <- c(averages, (sum / nOfRuns))
+    }
+    temp <- c(temp, averages)
+  }
+  result <- matrix(c(temp),nrow = nrow(baseParams),ncol = length(values))
+  write.table(result, file = fileName, row.names=FALSE, 
+              na="", col.names=FALSE, sep=";")
+  
+  if (graphs) {
+    
+    png(file = paste(funcName, graphName, ".png", sep=""), width=600, height=400, units="px")
+    
+    plot(0, 0, main=main,
+         ylim=c(min(c(temp,globalOpt)),max(c(temp,globalOpt))),
+         xlim=c(min(values),max(values)),
+         type="n", xlab=xlab, ylab="wartosc")
+    abline(globalOpt,0, col="green")
+    colorNames <- c()
+    seriesNames <- c()
+    for (i in 1:nrow(baseParams)) {
+      color <- colors[baseParams[i,5]]
+      colorNames <- c(colorNames, color)
+      seriesNames <- c(seriesNames, series[baseParams[i,5]])
+      lines(values, result[i,], col = color, type = 'l')
+    }
+    legend("topright", seriesNames, lty=rep(1,nrow(baseParams)), col=colorNames)
+
+    dev.off()
+    
+    summary(gBest)
+    
+    png(file = paste(funcName, graphName, mType, ".png", sep=""), width=600, height=400, units="px")
+    filled.contour(x, y, z, color.palette = jet.colors, nlevels = 24, 
+         plot.axes = { axis(1); axis(2);
+           points(solution[1,1], solution[1,2], 
+                  pch = 3, cex = 5, col = "black", lwd = 2) 
+         }
+    )
+    dev.off()
+    
+    png(file = paste(funcName, graphName, mType, "fitness", ".png", sep=""), width=600, height=400, units="px")
+    plot(gBest)
+    dev.off()
+  }
+  
+}
+
 dim <- getProblemDimen(funcName)
 B <- matrix(unlist(getDefaultBounds(funcName)),ncol=dim,byrow=TRUE)
-f <- function(xx) goTest(par=c(xx, rep(0, dim-length(xx))), fnName=funcName, checkDim = TRUE)
+f <- function(xx) goTest(par=c(xx, rep(0, dim-length(xx))), 
+                         fnName=funcName, checkDim = TRUE)
 globalOpt <- getGlobalOpt(funcName)
 
 if (graphs) {
+
   xprobes <- abs(B[2,1] - B[1,1]) / quality
   yprobes <- abs(B[2,2] - B[1,2]) / quality
   x <- seq(B[1,1], B[2,1], by = xprobes)
@@ -53,213 +134,25 @@ if (graphs) {
   zcol  = cut(z, nbcol)
   persp3d(x, y, z, theta=50, phi=25, expand=0.75, col=color[zcol],
           ticktype="detailed",axes=TRUE)
+  
+  png(file = paste(funcName, "1.png", sep=""), width=600, height=400, units="px")
   persp3D(x, y, z, theta = -45, phi = 20, color.palette = jet.colors)
-}
-
-if (isSingleTest) {
-
-  vector <- rep(NA,n)
-  for (i in 1:n) {
-    GAmin <- ga(type = "real-valued", fitness =  function(xx) -f(xx), 
-                min = c(B[1,]), max = c(B[2,]), 
-                popSize = GAPopulation, maxiter = GAIterations, 
-                pmutation = GAMutations, pcrossover = GACrossovers)
-    solution <- matrix(unlist(GAmin@solution),ncol=dim,byrow=TRUE)
-    vector[i] <- f(solution[1,])
-  }
-  result <- matrix(c(vector),nrow = n,ncol = 1)
-  write.table(result, file = "resultsSingle.csv", row.names=FALSE, na="", col.names=FALSE, sep=";")
-  
-} else {
-  
-  gMin <- .Machine$integer.max
-  gBest <- NA
-  
-  temp <- c()
-  values <- mutationTests
-  averages <- c()
-  for (mutation in values) {
-    sum <- 0
-    vector <- rep(NA,n)
-    for (i in 1:n) {
-      GAmin <- ga(type = "real-valued",
-                  fitness =  function(xx) -f(xx),
-                  min = c(B[1,]), max = c(B[2,]),
-                  popSize = GAPopulation, maxiter = GAIterations,
-                  pmutation = mutation, pcrossover = GACrossovers)
-      solution <- matrix(unlist(GAmin@solution),ncol=dim,byrow=TRUE)
-      eval <- f(solution[1,])
-      if (eval < gMin) {
-        gMin <- eval
-        gBest <- GAmin
-      }
-      sum <- sum + eval
-      vector[i] <- eval
-    }
-    temp <- c(temp, vector)
-    averages <- c(averages, (sum / n))
-  }
-  result <- matrix(c(temp),nrow = n,ncol = length(values))
-  write.table(result, file = "resultsMutations.csv", row.names=FALSE, na="", col.names=FALSE, sep=";")
-
-  if (graphs) {
-    plot(values, averages,
-         main="Function values for different mutation probabilities",
-         ylim=c(min(c(averages,globalOpt)),max(c(averages,globalOpt))),
-         type="l", col="red", xlab="params", ylab="value")
-    abline(globalOpt,0, col="green")
-  }
-
-  temp <- c()
-  values <- crossoverTests
-  averages <- c()
-  for (crossover in values) {
-    sum <- 0
-    vector <- rep(NA,n)
-    for (i in 1:n) {
-      GAmin <- ga(type = "real-valued",
-                  fitness =  function(xx) -f(xx),
-                  min = c(B[1,]), max = c(B[2,]),
-                  popSize = GAPopulation, maxiter = GAIterations,
-                  pmutation = GAMutations, pcrossover = crossover)
-      solution <- matrix(unlist(GAmin@solution),ncol=dim,byrow=TRUE)
-      eval <- f(solution[1,])
-      if (eval < gMin) {
-        gMin <- eval
-        gBest <- GAmin
-      }
-      sum <- sum + eval
-      vector[i] <- eval
-    }
-    temp <- c(temp, vector)
-    averages <- c(averages, (sum / n))
-  }
-  result <- matrix(c(temp),nrow = n,ncol = length(values))
-  write.table(result, file = "resultsCrossover.csv", row.names=FALSE, na="", col.names=FALSE, sep=";")
-
-  if (graphs) {
-    plot(values, averages,
-         main="Function values for different crossover probabilities",
-         ylim=c(min(c(averages,globalOpt)),max(c(averages,globalOpt))),
-         type="l", col="red", xlab="params", ylab="value")
-    abline(globalOpt,0, col="green")
-  }
-
-  temp <- c()
-  values <- elitismTests
-  averages <- c()
-  for (elitism in values) {
-    sum <- 0
-    vector <- rep(NA,n)
-    for (i in 1:n) {
-      GAmin <- ga(type = "real-valued",
-                  fitness =  function(xx) -f(xx),
-                  min = c(B[1,]), max = c(B[2,]),
-                  popSize = GAPopulation, maxiter = GAIterations,
-                  pmutation = GAMutations, pcrossover = GACrossovers, elitism = elitism)
-      solution <- matrix(unlist(GAmin@solution),ncol=dim,byrow=TRUE)
-      eval <- f(solution[1,])
-      if (eval < gMin) {
-        gMin <- eval
-        gBest <- GAmin
-      }
-      sum <- sum + eval
-      vector[i] <- eval
-    }
-    temp <- c(temp, vector)
-    averages <- c(averages, (sum / n))
-  }
-  result <- matrix(c(temp),nrow = n,ncol = length(values))
-  write.table(result, file = "resultsElitism.csv", row.names=FALSE, na="", col.names=FALSE, sep=";")
-
-  if (graphs) {
-    plot(values, averages,
-         main="Function values for different elitism",
-         ylim=c(min(c(averages,globalOpt)),max(c(averages,globalOpt))),
-         type="l", col="red", xlab="params", ylab="value")
-    abline(globalOpt,0, col="green")
-  }
-
-  temp <- c()
-  values <- populationTests
-  averages <- c()
-  for (population in values) {
-    sum <- 0
-    vector <- rep(NA,n)
-    for (i in 1:n) {
-      GAmin <- ga(type = "real-valued", 
-                  fitness =  function(xx) -f(xx), 
-                  min = c(B[1,]), max = c(B[2,]), 
-                  popSize = population, maxiter = GAIterations, 
-                  pmutation = GAMutations, pcrossover = GACrossovers)
-      solution <- matrix(unlist(GAmin@solution),ncol=dim,byrow=TRUE)
-      eval <- f(solution[1,])
-      if (eval < gMin) {
-        gMin <- eval
-        gBest <- GAmin
-      }
-      sum <- sum + eval
-      vector[i] <- eval
-    }
-    temp <- c(temp, vector)
-    averages <- c(averages, (sum / n))
-  }
-  result <- matrix(c(temp),nrow = n,ncol = length(values))
-  write.table(result, file = "resultsPopulation.csv", row.names=FALSE, na="", col.names=FALSE, sep=";")
-
-  if (graphs) {
-    plot(values, averages,
-         main="Function values for different population sizes", 
-         ylim=c(min(c(averages,globalOpt)),max(c(averages,globalOpt))), 
-         type="l", col="red", xlab="params", ylab="value")
-    abline(globalOpt,0, col="green")
-  }
-
-  temp <- c()
-  values <- iterationTests
-  averages <- c()
-  for (iterations in values) {
-    sum <- 0
-    vector <- rep(NA,n)
-    for (i in 1:n) {
-      GAmin <- ga(type = "real-valued", 
-                  fitness =  function(xx) -f(xx), 
-                  min = c(B[1,]), max = c(B[2,]), 
-                  popSize = GAPopulation, maxiter = iterations, 
-                  pmutation = GAMutations, pcrossover = GACrossovers)
-      solution <- matrix(unlist(GAmin@solution),ncol=dim,byrow=TRUE)
-      eval <- f(solution[1,])
-      if (eval < gMin) {
-        gMin <- eval
-        gBest <- GAmin
-      }
-      sum <- sum + eval
-      vector[i] <- eval
-    }
-    temp <- c(temp, vector)
-    averages <- c(averages, (sum / n))
-  }
-  result <- matrix(c(temp),nrow = n,ncol = length(values))
-  write.table(result, file = "resultsIterations.csv", row.names=FALSE, na="", col.names=FALSE, sep=";")
-
-  if (graphs) {
-    plot(values, averages,
-         main="Function values for different number of iterations", 
-         ylim=c(min(c(averages,globalOpt)),max(c(averages,globalOpt))), 
-         type="l", col="red", xlab="params", ylab="value")
-    abline(globalOpt,0, col="green")
-  }
+  dev.off()
   
 }
 
-if (graphs) {
-  summary(GAmin)
-  filled.contour(x, y, z, color.palette = jet.colors, nlevels = 24, 
-     plot.axes = { 
-       axis(1); 
-       axis(2);
-       points(solution[1,1], solution[1,2], pch = 3, cex = 5, col = "black", lwd = 2) 
-     }
-  )
-  plot(GAmin)
-}
+customMeasure("resultsMutations.csv", "2", mutationTests, "mut", "p. mutacji",
+              "Znalezione minimum dla roznych prawdopodobienstw mutacji")
+
+customMeasure("resultsCrossover.csv", "3", crossoverTests, "crs", "p. krzyzowania",
+              "Znalezione minimum dla roznych prawdopodobienstw krzyzowania")
+
+customMeasure("resultsPopulation.csv", "4", populationTests, "pop", "rozmiar populacji",
+              "Znalezione minimum dla roznych rozmiarow populacji")
+
+customMeasure("resultsIterations.csv", "5", iterationTests, "itr","ilosc iteracji",
+              "Znalezione minimum dla roznych ilosci iteracji")
+
+customMeasure("resultsElitism.csv", "6", elitismTests, "elt", "elityzm",
+              "Znalezione minimum dla roznych wartosci elityzmu")
+
