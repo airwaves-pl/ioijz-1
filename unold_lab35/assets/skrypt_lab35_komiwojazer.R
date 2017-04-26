be24b03ce4b8318959993c2e7a8b68a55a436cc2
marcin-pwr <200638@student.pwr.edu.pl>
2017-04-26 14:01:46 +0200

    komiwojażer improvements

--- unold_lab35/assets/skrypt_lab35_komiwojazer.R
+++ unold_lab35/assets/skrypt_lab35_komiwojazer.R
@@ -9,61 +9,130 @@ require("rgl")
 require("TSP")
 require("psoptim")
 
+numberOfMeasurements <- 2
+
+
 # TSP with GA ----
 
+# instances to test and best known solutions
 instances <- c("eil51", "eil76", "eil101")
 best_solutions <- c(426, 538, 629)
-found_solutions <- c()
-solutions_quality <- c()
+colors <- c("red", "green", "blue")
 
-for (i in 1:length(instances)) {
-  
-  fileName = paste("examples/", instances[i], ".tsp", sep="")
-  graphTitle = paste("TSPLIB: ", instances[i], sep="")
-  
-  drill <- read_TSPLIB(system.file(fileName, package = "TSP"))
-  D <- as.matrix(dist(drill, method = "euclidean"))
-  N <- max(dim(D))
-  
-  tourLength <- function(tour, distMatrix) {
-    tour <- c(tour, tour[1])
-    route <- embed(tour, 2)[,2:1]
-    sum(distMatrix[route])
-  }
-  
-  fit <- function(tour, distMatrix) 1/tourLength(tour, distMatrix)
-  
-  GA <- ga(type = "permutation", 
-           fitness = fit, 
-           distMatrix = D, 
-           min = 1, 
-           max = N, 
-           maxiter=200, 
-           pmutation=0.2, 
-           run=300, 
-           optim = FALSE)
-  
-  tour <- GA@solution[1, ]
-
-  plot(drill, tour, cex=.6, col = "red", pch= 3, main = graphTitle)
-  
-  tl <- tourLength(tour, D)
-  ol <- best_solutions[i]
-  
-  found_solutions <- c(found_solutions, tl)
-  solutions_quality <- c(solutions_quality, (ol/tl) * 100)
-
+tourLength <- function(tour, distMatrix) {
+  tour <- c(tour, tour[1])
+  route <- embed(tour, 2)[,2:1]
+  sum(distMatrix[route])
 }
 
-png(file = "tsp_results.png", width=600, height=400, units="px")
-plot(1:length(instances), xaxt = "n", solutions_quality, col="red", main="Wyniki dla ró¿nych instancji", type="l", xlab="instancje", ylab="jakoœæ rozwi¹zañ [%]")
-axis(1, at=1:length(instances), labels=instances)
-dev.off()
+fit <- function(tour, distMatrix) 1/tourLength(tour, distMatrix)
+
+performTest <- function(testName, graphMain, graphXLab, 
+                        sequenceType, sequence, 
+                        popsize=50, pcrossover=0.8, 
+                        pmutation=0.1, maxiter=100,
+                        optim=FALSE) {
+  
+  solution_qualities <- c()
+  
+  # each instance as separate serie
+  for (i in 1:length(instances)) {
+    
+    fileName = paste("examples/", instances[i], ".tsp", sep="")
+    graphTitle = paste("TSPLIB: ", instances[i], sep="")
+    
+    drill <- read_TSPLIB(system.file(fileName, package = "TSP"))
+    D <- as.matrix(dist(drill, method = "euclidean"))
+    N <- max(dim(D))
+    
+    solution_quality <- c()
+    
+    for (i in 1:length(sequence)) {
+      
+      bestTour <- NA
+      bestTourLength <- .Machine$integer.max
+      averageLength <- 0
+      
+      for (i in 1:numberOfMeasurements) {
+        
+        GA <- ga(type = "permutation", 
+                 fitness = fit, 
+                 distMatrix = D, 
+                 min = 1, 
+                 max = N, 
+                 popSize = if (sequenceType == "popsize") sequence[i] else popsize, 
+                 pcrossover = if (sequenceType == "pcrossover") sequence[i] else pcrossover, 
+                 pmutation = if (sequenceType == "pmutation") sequence[i] else pmutation, 
+                 maxiter = if (sequenceType == "maxiter") sequence[i] else maxiter,
+                 optim = optim)
+        
+        tour <- GA@solution[1, ]
+        tl <- tourLength(tour, D)
+        
+        if (tl < bestTourLength) {
+          bestTourLength <- tl
+          bestTour <- tour
+        }
+        
+        averageLength <- averageLength + (tl - averageLength) / i
+        
+      }
+      
+      plot(drill, tour, cex=.6, col = "red", pch=3, main = graphTitle)
+      
+      solution_quality <- c(solution_quality, 
+                            (best_solutions[i]/averageLength) * 100)
+      
+    }
+
+    solution_qualities <- c(solution_qualities, solution_quality)
+    
+  }
+  
+  
+  qualities = matrix(solution_qualities, 
+                  nrow=length(instances), ncol=length(sequence), byrow = TRUE)
+  
+  # save graph with measurement series to file
+  png(file = paste(testName, ".png", sep=""), width=600, height=400, units="px")
+  plot(0, 0, main=graphMain, 
+       ylim=c(0,100),
+       xlim=c(min(sequence),max(sequence)),
+       type="n", xlab=graphXLab, ylab="jakoœæ rozwi¹zañ [%]")
+  for (i in 1:length(instances)) {
+    lines(sequence, qualities[i,], col = colors[i], type = 'l')
+  }
+  legend("topright", instances, lwd=rep(2,length(instances)), lty=rep(1,length(instances)), col=colors)
+  dev.off()
+  
+}
+
+performTest(testName = "tsp_pop", 
+            graphMain = "Pomiary dla ró¿nych rozmiarów populacji", 
+            graphXLab = "rozmiar populacji", 
+            sequenceType = "popsize", sequence = seq(10, 100, 5))
+
+performTest(testName = "tsp_pop_hyb", 
+            graphMain = "Pomiary dla ró¿nych rozmiarów populacji (hybrydowy)", 
+            graphXLab = "rozmiar populacji", 
+            sequenceType = "popsize", sequence = seq(10, 100, 5), optim = TRUE)
+
+performTest(testName = "tsp_mut", 
+            graphMain = "Pomiary dla ró¿nych p. mutacji", 
+            graphXLab = "p. mutacji", 
+            sequenceType = "pmutation", sequence = seq(0, 1, 0.1))
+
+performTest(testName = "tsp_cross", 
+            graphMain = "Pomiary dla ró¿nych p. krzy¿owania", 
+            graphXLab = "p. krzy¿owania", 
+            sequenceType = "pcrossover", sequence = seq(0, 1, 0.1))
+
+
 
 
 # PSO tests ----
 
-n <- 50
+n <- 500
 m.l <- 50
 w <- 0.95
 c1 <- 0.2
@@ -73,8 +142,10 @@ xmax <- c(5.12, 5.12)
 vmax <- c(4, 4)
 
 g <- function(x){  
-  -(20 + x[,1]^2 + x[,2]^2 - 10*(cos(2*pi*x[,1]) + cos(2*pi*x[,2])))
+  -(200 + x[,1]^2 + x[,2]^2 + cos(2*pi*x[,2]))
 }
 
 psoptim(FUN=g, n=n, max.loop=m.l, w=w, c1=c1, c2=c2,
-        xmin=xmin, xmax=xmax, vmax=vmax, seed=5, anim=FALSE)
+        xmin=xmin, xmax=xmax, vmax=vmax, seed=5, anim=TRUE)
+
+#determine permutation by number
