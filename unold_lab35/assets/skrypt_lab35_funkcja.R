3b79712fc206b9549bd2bc5b9cb3dee6f351b5c2
Paweł A <ruri0000@gmail.com>
2017-05-03 20:48:13 +0200

    Wyniki c1, c2 dla PSO

--- unold_lab35/assets/skrypt_lab35_funkcja.R
+++ unold_lab35/assets/skrypt_lab35_funkcja.R
@@ -29,7 +29,7 @@ myMutationFunction <- function(object, parent) {
 
 # Settings ----
 
-nOfRuns <- 15 # 30 number of runs to calc avg scores
+nOfRuns <- 10 # 30 number of runs to calc avg scores
 
 # colors and titles for plot series
 colors <- c("red", "purple")
@@ -51,6 +51,28 @@ pressel = 0.5 #a value [0,1] specifying the pressure selection (def 0.5)
 
 # Processing ----
 
+{
+  # get data from globalOptTests package
+  dim <- getProblemDimen(funcName)
+  B <- matrix(unlist(getDefaultBounds(funcName)),ncol=dim,byrow=TRUE)
+  f <- function(xx) goTest(par=c(xx, rep(0, dim-length(xx))), 
+                           fnName=funcName, checkDim = TRUE)
+  globalOpt <- getGlobalOpt(funcName)
+  
+  if (graphs) {
+    # prepare overview graph
+    xprobes <- abs(B[2,1] - B[1,1]) / quality
+    yprobes <- abs(B[2,2] - B[1,2]) / quality
+    x <- seq(B[1,1], B[2,1], by = xprobes)
+    y <- seq(B[1,2], B[2,2], by = yprobes)
+    z <- outer(x, y, Vectorize(function(x,y) f(c(x,y))))
+    png(file = paste(funcName, "_overview.png", sep=""), width=600, height=400, units="px")
+    persp3D(x, y, z, theta = -45, phi = 20, color.palette = jet.colors)
+    dev.off()
+  }
+}
+
+#GA
 customGAMeasure <- function(values, mType, xlab, main) {
   
   # main measurement loop (for each serie and sequence calculate average results)
@@ -101,6 +123,45 @@ customGAMeasure <- function(values, mType, xlab, main) {
     legend("topright", seriesNames, lwd=rep(2,length(series)), lty=rep(1,length(series)), col=colorNames)
     dev.off()
     
+  }
+}
+
+customHybridMeasure <- function(values, mType, xlab, main) {
+  
+  averages <- c()
+  for (value in values) {
+    sum <- 0
+    for (i in 1:nOfRuns) {
+      
+      message(paste("Sekwencja: ", value))
+      message(paste("Przebieg: ", i))
+      
+      GAmin <- ga(type = "real-valued",
+                  fitness =  function(xx) -f(xx),
+                  min = c(B[1,]), max = c(B[2,]),
+                  optim = TRUE,
+                  optimArgs = list (
+                    poptim = if (mType == "poptim") value else 0.05, 
+                    pressel = if (mType == "pressel") value else 0.5))
+      solution <- matrix(unlist(GAmin@solution),ncol=dim,byrow=TRUE)
+      eval <- f(solution[1,])
+      sum <- sum + eval
+    }
+    averages <- c(averages, (sum / nOfRuns))
+  }
+  
+  if (graphs) {
+    
+    # save graph with measurement series to file
+    png(file = paste(funcName, mType, ".png", sep=""), width=600, height=400, units="px")
+    plot(0, 0, main=main,
+         ylim=c(min(c(averages,globalOpt)),max(c(averages,globalOpt))),
+         xlim=c(min(values),max(values)),
+         type="n", xlab=xlab, ylab="wartość")
+    abline(globalOpt,0, col="green")
+    lines(values, averages, col = "red", type = 'l')
+    legend("topright", c("memetyczny"), lwd=rep(2,1), lty=rep(1,1), col=c("red"))
+    dev.off()
   }
 }
 
@@ -179,95 +240,9 @@ customMeasureGAWithHybrid <- function(values, mType, xlab, main) {
   }
 }
 
-{
-# get data from globalOptTests package
-dim <- getProblemDimen(funcName)
-B <- matrix(unlist(getDefaultBounds(funcName)),ncol=dim,byrow=TRUE)
-f <- function(xx) goTest(par=c(xx, rep(0, dim-length(xx))), 
-						 fnName=funcName, checkDim = TRUE)
-globalOpt <- getGlobalOpt(funcName)
+#PSO
 
-if (graphs) {
-  # prepare overview graph
-  xprobes <- abs(B[2,1] - B[1,1]) / quality
-  yprobes <- abs(B[2,2] - B[1,2]) / quality
-  x <- seq(B[1,1], B[2,1], by = xprobes)
-  y <- seq(B[1,2], B[2,2], by = yprobes)
-  z <- outer(x, y, Vectorize(function(x,y) f(c(x,y))))
-  png(file = paste(funcName, "_overview.png", sep=""), width=600, height=400, units="px")
-  persp3D(x, y, z, theta = -45, phi = 20, color.palette = jet.colors)
-  dev.off()
-}
-}
-
-
-# perform set of measurements ----
-customMeasureGAWithHybrid(seq(0, 1, 0.01), "mut", "p.mutacji", "Znalezione minimum dla różnych p. mutacji")
-
-customGAMeasure(seq(0, 1, 0.1), "mut", 
-	"p. mutacji", "Znalezione minimum dla różnych p. mutacji")
-customGAMeasure(seq(10, 100, 10), "pop", 
-	"rozmiar populacji", "Znalezione minimum dla różnych rozmiarów populacji")
-
-
-
-# hybrid algorithm ----
-
-customHybridMeasure <- function(values, mType, xlab, main) {
-
-  averages <- c()
-  for (value in values) {
-    sum <- 0
-    for (i in 1:nOfRuns) {
-      
-      message(paste("Sekwencja: ", value))
-      message(paste("Przebieg: ", i))
-      
-      GAmin <- ga(type = "real-valued",
-                  fitness =  function(xx) -f(xx),
-                  min = c(B[1,]), max = c(B[2,]),
-                  optim = TRUE,
-                  optimArgs = list (
-                    poptim = if (mType == "poptim") value else 0.05, 
-                    pressel = if (mType == "pressel") value else 0.5))
-      solution <- matrix(unlist(GAmin@solution),ncol=dim,byrow=TRUE)
-      eval <- f(solution[1,])
-      sum <- sum + eval
-    }
-    averages <- c(averages, (sum / nOfRuns))
-  }
-
-  if (graphs) {
-    
-    # save graph with measurement series to file
-    png(file = paste(funcName, mType, ".png", sep=""), width=600, height=400, units="px")
-    plot(0, 0, main=main,
-         ylim=c(min(c(averages,globalOpt)),max(c(averages,globalOpt))),
-         xlim=c(min(values),max(values)),
-         type="n", xlab=xlab, ylab="wartość")
-    abline(globalOpt,0, col="green")
-    lines(values, averages, col = "red", type = 'l')
-    legend("topright", c("memetyczny"), lwd=rep(2,1), lty=rep(1,1), col=c("red"))
-    dev.off()
-  }
-}
-
-customHybridMeasure(seq(0, 1, 0.01), "poptim", 
-                "p. lokalnego searcha", "Znalezione minimum dla różnych poptimów")
-customHybridMeasure(seq(0, 1, 0.01), "pressel", 
-                    "ciśnienie", "Znalezione minimum dla różnych ciśnień")
-
-
-# PSO tests ----
-
-
-nOfRuns = 1 # zostaje bo niby nie można uśredniać?
-
-
-#TODO
-  
-customPSOMeasure <- function(values, valueType, xLabel, title)
-{
+customPSOMeasure <- function(values, valueType, xLabel, title) {
   n <- 500 #ilosc czastek
   m.l <- 50 #ilosc przebiegow
   w <- 0.95
@@ -315,9 +290,33 @@ customPSOMeasure <- function(values, valueType, xLabel, title)
          type="n", xlab=xLabel, ylab="wartość")
     abline(globalOpt,0, col="green")
     lines(values, averages, col = "red", type = 'l')
-    legend("topright", c("PSO"), lwd=rep(2,1), lty=rep(1,1), col=c("red"))
+    legend("bottomright", c("PSO"), lwd=rep(2,1), lty=rep(1,1), col=c("red"))
     dev.off()
   }
 }
-  
-customPSOMeasure(seq(0, 1, 0.01), "c1" ,"c1", "Znalezione minimum dla różnych wartości c1")
+
+
+
+# perform set of measurements ----
+# GA
+customGAMeasure(seq(0, 1, 0.1), "mut", 
+                "p. mutacji", "Znalezione minimum dla różnych p. mutacji")
+
+customGAMeasure(seq(10, 100, 10), "pop", 
+                "rozmiar populacji", "Znalezione minimum dla różnych rozmiarów populacji")
+
+# Hybrid
+customHybridMeasure(seq(0, 1, 0.01), "poptim", 
+                    "p. lokalnego searcha", "Znalezione minimum dla różnych poptimów")
+
+customHybridMeasure(seq(0, 1, 0.01), "pressel", 
+                    "ciśnienie", "Znalezione minimum dla różnych ciśnień")
+
+# Mixed (GA+Hybrid)
+customMeasureGAWithHybrid(seq(0, 1, 0.01), "mut", 
+                          "p.mutacji", "Znalezione minimum dla różnych p. mutacji")
+
+
+# PSO
+customPSOMeasure(seq(0, 1, 0.01), "c1" ,
+                 "c1", "Znalezione minimum dla różnych wartości c1")
