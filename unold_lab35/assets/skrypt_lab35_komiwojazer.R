1870d8715bbb247754a71ae72bf2a7afc3edee1d
marcin-pwr <200638@student.pwr.edu.pl>
2017-05-06 22:25:48 +0200

    poprawki sprawka

--- unold_lab35/assets/skrypt_lab35_komiwojazer.R
+++ unold_lab35/assets/skrypt_lab35_komiwojazer.R
@@ -9,9 +9,9 @@ require("rgl")
 require("TSP")
 require("psoptim")
 
-numberOfMeasurements <- 1 #15
+numberOfMeasurements <- 15
 
-# instances to test and best known solutions
+# instances for testing and best known solutions
 instances <- c("eil51", "eil76", "eil101")
 best_solutions <- c(426, 538, 629)
 colors <- c("red", "green", "blue")
@@ -25,8 +25,7 @@ tourLength <- function(tour, distMatrix) {
 fit <- function(tour, distMatrix) 1/tourLength(tour, distMatrix)
 
 customMutation <- function(object, parent, ...) {
-  
-  # Insertion mutation
+  # insertion mutation
   parent <- as.vector(object@population[parent,])
   n <- length(parent)
   m <- sample(1:n, size = 1)
@@ -34,7 +33,7 @@ customMutation <- function(object, parent, ...) {
   i <- c(setdiff(1:pos,m), m, setdiff((pos+1):n,m))
   mutate <- parent[i]
 
-  # Displacement mutation
+  # displacement mutation
   parent <- mutate
   m <- sort(sample(1:n, size = 2))
   m <- seq(m[1], m[2], by = 1)
@@ -43,7 +42,7 @@ customMutation <- function(object, parent, ...) {
   i <- c(setdiff(1:n,m)[1:pos], m, setdiff(1:n,m)[-(1:pos)])
   mutate <- parent[na.omit(i)]
   
-  # Scramble mutation
+  # scramble mutation
   parent <- mutate
   m <- sort(sample(1:n, size = 2))
   m <- seq(min(m), max(m), by = 1)
@@ -51,7 +50,6 @@ customMutation <- function(object, parent, ...) {
   i <- c(setdiff(1:min(m),m), m, setdiff(max(m):n,m))
   mutate <- parent[i]
   return(mutate)
-  
 } 
 
 performTest <- function(testName, graphMain, graphXLab, 
@@ -63,7 +61,6 @@ performTest <- function(testName, graphMain, graphXLab,
   
   # each instance as separate serie
   for (i in 1:length(instances)) {
-    
     fileName = paste("examples/", instances[i], ".tsp", sep="")
     graphTitle = paste("TSPLIB: ", instances[i], sep="")
     
@@ -72,15 +69,11 @@ performTest <- function(testName, graphMain, graphXLab,
     N <- max(dim(D))
     
     solution_quality <- c()
-    
     bestTour <- NA
     bestTourLength <- .Machine$integer.max
     averageLength <- 0
-    
     for (s in 1:length(sequence)) {
-      
       for (n in 1:numberOfMeasurements) {
-        
         message(paste("Instancja: ", i))
         message(paste("Sekwencja: ", s))
         message(paste("Pomiar: ", n))
@@ -98,19 +91,14 @@ performTest <- function(testName, graphMain, graphXLab,
         
         tour <- GA@solution[1, ]
         tl <- tourLength(tour, D)
-        
         if (tl < bestTourLength) {
           bestTourLength <- tl
           bestTour <- tour
         }
-        
         averageLength <- averageLength + (tl - averageLength) / n
-        
       }
-      
       solution_quality <- c(solution_quality, 
                             (best_solutions[i]/averageLength) * 100)
-      
     }
 
     png(file = paste(testName, "_", instances[i], ".png", sep=""), width=600, height=400, units="px")
@@ -118,13 +106,9 @@ performTest <- function(testName, graphMain, graphXLab,
     dev.off()
     
     solution_qualities <- c(solution_qualities, solution_quality)
-    
   }
   
-  
-  qualities = matrix(solution_qualities, 
-                  nrow=length(instances), ncol=length(sequence), byrow = TRUE)
-  
+  qualities = matrix(solution_qualities, nrow=length(instances), ncol=length(sequence), byrow = TRUE)
   # save graph with measurement series to file
   png(file = paste(testName, ".png", sep=""), width=600, height=400, units="px")
   plot(0, 0, main=graphMain, 
@@ -136,21 +120,16 @@ performTest <- function(testName, graphMain, graphXLab,
   }
   legend("topright", instances, lwd=rep(2,length(instances)), lty=rep(1,length(instances)), col=colors)
   dev.off()
-  
 }
 
-performTest(testName = "tsp_pop", 
-            graphMain = "Pomiary dla różnych rozmiarów populacji", 
+performTest(testName = "tsp_pop", graphMain = "Pomiary dla różnych rozmiarów populacji", 
             graphXLab = "rozmiar populacji", 
             sequenceType = "popsize", sequence = seq(50, 500, 50))
 
-performTest(testName = "tsp_mut", 
-            graphMain = "Pomiary dla różnych p. mutacji", 
+performTest(testName = "tsp_mut", graphMain = "Pomiary dla różnych p. mutacji", 
             graphXLab = "p. mutacji", 
             sequenceType = "pmutation", sequence = seq(0, 1, 0.1))
 
-performTest(testName = "tsp_mut_custom", 
-            graphMain = "Pomiary dla różnych p. mutacji (własny op. mutacji)", 
+performTest(testName = "tsp_mut_custom", graphMain = "Pomiary dla różnych p. mutacji (własny op. mutacji)", 
             graphXLab = "p. mutacji", 
             sequenceType = "pmutation", sequence = seq(0, 1, 0.1), mutation = customMutation)
-
