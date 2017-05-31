e8430623b92ad3abd18f0cf00e70bdfc4bd10594
marcin-pwr <200638@student.pwr.edu.pl>
2017-05-31 09:50:57 +0200

    yalefaces + zad1 legend

--- zdunek_cw1/assets/skrypt_zad1.m
+++ zdunek_cw1/assets/skrypt_zad1.m
@@ -1,8 +1,9 @@
 
 x = [2.5 0.5 2.2 1.9 3.1 2.3 2 1 1.5 1.1; 2.4 0.7 2.9 2.2 3 2.7 1.6 1.1 1.6 0.9];
 [eigenvectors, eigenvalues] = eigs(x*(x'), 2);
-m = (x') * eigenvectors;
+pcs = (x') * eigenvectors;
 hold on
-plot(x(1:1,:), x(2:2,:), 'or', m(:,1:1), m(:,2:2), 'ob')
+plot(x(1:1,:), x(2:2,:), 'or', pcs(:,1:1), pcs(:,2:2), 'ob')
 plotv(eigenvectors(:,1), '-r');
 plotv(eigenvectors(:,2), '-b');
+legend('dane orginalne','sk³adowe g³ówne','wektor cechy 1','wektor cechy 2','Location','northwest','Orientation','vertical')
--- zdunek_cw1/assets/skrypt_zad2.m
+++ zdunek_cw1/assets/skrypt_zad2.m
@@ -7,3 +7,29 @@ X=[randn(200,2);randn(200,2)+6,;[randn(200,1)+12,randn(200,1)]]; T=[ones(200,1);
  idx=kmeans(X,3,'emptyaction','singleton','Replicates',5); 
   [Acc,rand_index,match]=AccMeasure(T,idx)
   
+close all;
+load yalefaces
+[h,w,n] = size(yalefaces);
+d = h*w;
+% vectorize images
+x = reshape(yalefaces,[d n]);
+x = double(x);
+%subtract mean
+x=bsxfun(@minus, x, mean(x,2));
+% calculate covariance
+s = cov(x');
+% obtain eigenvalue & eigenvector
+[V,D] = eig(s);
+eigval = diag(D);
+% sort eigenvalues in descending order
+eigval = eigval(end:-1:1);
+V = fliplr(V);
+% show 0th through 15th principal eigenvectors
+eig0 = reshape(mean(x,2), [h,w]);
+figure,subplot(4,4,1)
+imagesc(eig0)
+colormap gray
+for i = 1:15
+subplot(4,4,i+1)
+imagesc(reshape(V(:,i),h,w))
+end
