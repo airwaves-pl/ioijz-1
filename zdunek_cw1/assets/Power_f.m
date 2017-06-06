9a6a85da107371addda9e7387ae3d0bebcb1adb4
Paweł A <ruri0000@gmail.com>
2017-06-07 01:49:13 +0200

    Funkcja powera - dwa argumenty (macierz, tolerancja)

---
+++ zdunek_cw1/assets/Power_f.m
@@ -0,0 +1,21 @@
+function [new_result] = Power_f(X, tolerance)
+
+    n = size(X);
+    n = n(1);
+    
+    new_result = ones(n,1);
+    m = 0;
+    
+    while(1)
+        mold = m;
+        old_result = new_result;    
+        new_result = X * new_result;
+
+        m = max(new_result);
+        new_result = new_result/m;
+        
+        if abs(m-mold) < tolerance && norm(new_result-old_result,2) < tolerance
+            break;
+        end
+    end
+end
