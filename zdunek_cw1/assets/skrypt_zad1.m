cc5f3f01be21028f72c23f2c9055ec58cfa4b27e
Robert <robek.chojnacki@gmail.com>
2017-06-07 10:00:51 +0200

    Poprawka skryptu z zad 1. Sprawozdanie - opis algorytmu PCA

--- zdunek_cw1/assets/skrypt_zad1.m
+++ zdunek_cw1/assets/skrypt_zad1.m
@@ -1,12 +1,20 @@
-
-x = [2.5 0.5 2.2 1.9 3.1 2.3 2 1 1.5 1.1; 2.4 0.7 2.9 2.2 3 2.7 1.6 1.1 1.6 0.9];
-s = cov(x');
+% dane wejsciowe
+x = [2.5 0.5 2.2 1.9 3.1 2.3 2 1 1.5 1.1;
+    2.4 0.7 2.9 2.2 3 2.7 1.6 1.1 1.6 0.9];
+% obliczenie wartosci srednich
+% odjecie ich od macierzy
+x_norm = bsxfun(@minus, x, mean(x, 2));
+% wyznaczenie macierzy kowariancji
+s = cov(x_norm');
+% wyznaczenie wartosci i wektorow wlasnych
 [eigenvectors, eigenvalues] = eigs(s, 2);
-pcs = (x') * eigenvectors;
+% rzutowanie
+pcs = eigenvectors' * x;
+
 figure;
 hold on
-plot(x(1:1,:), x(2:2,:), 'or', pcs(:,1:1), pcs(:,2:2), 'ob')
-plotv(eigenvectors(:,1), '-r');
-plotv(eigenvectors(:,2), '-b');
-legend('dane oryginalne','składowe główne','wektor cechy 1','wektor cechy 2','Location','northwest','Orientation','vertical')
+plot(x(1,:), x(2,:), 'or', pcs(1,:), pcs(2,:), 'ob')
+plotv(eigenvectors(:,1), '-');
+plotv(eigenvectors(:,2), '-');
+legend('dane oryginalne', 'skladowe glowne', 'wektor cechy 1','wektor cechy 2','Location','northeast','Orientation','vertical')
 hold off
