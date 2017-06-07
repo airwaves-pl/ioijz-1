d096bab023db3001f91347f8a62a04180ecc7668
Paweł A <ruri0000@gmail.com>
2017-06-07 11:42:26 +0200

    Opis algorytmu Powera

--- zdunek_cw1/assets/Power_f.m
+++ zdunek_cw1/assets/Power_f.m
@@ -7,14 +7,14 @@ function [new_result] = Power_f(X, tolerance)
     m = 0;
     
     while(1)
-        mold = m;
+        m_old = m;
         old_result = new_result;    
         new_result = X * new_result;
 
         m = max(new_result);
         new_result = new_result/m;
         
-        if abs(m-mold) < tolerance && norm(new_result-old_result,2) < tolerance
+        if abs(m-m_old) < tolerance && norm(new_result-old_result,2) < tolerance
             break;
         end
     end
