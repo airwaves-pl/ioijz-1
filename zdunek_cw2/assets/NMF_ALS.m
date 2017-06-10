86b5849b01ac4aaf1318a334fc75a136b49fc145
Paweł A <ruri0000@gmail.com>
2017-06-11 01:32:16 +0200

    zad 1 ALS, MUE, HALS(prawie), rysowanie wszystkich na wykresie

---
+++ zdunek_cw2/assets/NMF_ALS.m
@@ -0,0 +1,15 @@
+function[A,X,res] = NMF_ALS(A,X,Y,N)
+
+    for k = 1:N
+        % obliczanie A
+        A = max(0,Y*X'*inv(X*X'));
+        A = A*diag(1./sum(A,1));
+
+        % obliczanie X
+        X = max(0,inv(A'*A)*A'*Y);
+        
+        % b³¹d residualny
+        res(k) = norm(Y - A*X,'fro')/norm(Y,'fro');
+
+    end
+end
---
+++ zdunek_cw2/assets/NMF_MUE.m
@@ -0,0 +1,18 @@
+function [A,X,res] = MUE(A,X,Y,N)
+  for k = 1:N
+    % obliczanie A
+    s1 = A.*(Y*X');
+    s2 = (A*X*X');
+    
+    A = max(0, s1./s2);  
+    A = A*diag(1./sum(A,1));
+    
+    % obliczanie X
+    s1 = X.*(A'*Y);
+    s2 =A'*A*X;
+    
+    X = max(0, s1./s2);
+    
+    % b³¹d residualny
+    res(k) = norm(Y - A*X, 'fro')/norm(Y,'fro');
+  end
